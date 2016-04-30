835d893c1251c3cf27f7ce6457014e04c55ecf5d
Daniel Zuluaga <dzszuluaga@gmail.com>
2016-04-30 15:19:47 -0500

    Working

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,23 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+## Usage:
+## m <- matrix(c(1, 2, 3, 4), nrow=2, ncol=2)
+## cacheMatrix <- makeCacheMatrix(m)
+## cacheSolve(cacheMatrix)
 
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 
 
@@ -14,13 +29,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ## from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmean(m)
+    m
 }
 
 ## Usage:
 ## m <- matrix(c(1, 2, 3, 4), nrow=2, ncol=2)
-## cacheMatrix <- makeVector(m)
-## cachemean(cacheMatrix)
+## cacheVector <- makeVector(m)
+## cachemean(cacheVector)
 
 makeVector <- function(x = numeric()) {
     m <- NULL
